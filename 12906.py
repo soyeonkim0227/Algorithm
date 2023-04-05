40925f4bc7fbcc1aef270d81d39345b5e072c9f3
soyeonkim0227 <soyeonkim0227@naver.com>
2023-04-05 20:14:50 +0900

    프로그래머스 12906번 같은 숫자는 싫어

--- 12906.py
+++ 12906.py
@@ -1,6 +1,6 @@
 def solution(arr):
     answer = []
-    for i in range(0, len(arr)+1):
-        if arr[i] not in arr[i-1]:
-            answer.append(arr[i])
-            print(answer)
+    for i in arr:
+        if not answer or i != answer[-1]:
+            answer.append(i)
+    return answer
