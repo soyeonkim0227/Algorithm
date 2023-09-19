827402104d026964b73ddc991f1935e10d30d625
soyeonkim0227 <102784901+soyeonkim0227@users.noreply.github.com>
2023-09-19 22:22:00 +0900

    [Bronze V] Title: 개수 세기, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -1,11 +1,5 @@
-import sys
+num = int(input())
+data = list(map(int, input().split()))
+search = int(input())
 
-num = int(sys.stdin.readline())
-data = list(map(int, sys.stdin.readline().split()))
-search = int(sys.stdin.readline())
-cnt = 0
-
-for i in range(num):
-    if data[i] == search:
-        cnt += 1
-print(cnt)
+print(data.count(search))
